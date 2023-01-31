4ac8588a8d1cd7535827ff23aa8ff9cd6f192c74
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2023-01-31 16:43:29 +0300

    Summary

---
+++ solutions/python/medium/6__Zigzag_Conversion.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+6. Zigzag Conversion
+"""
+
+# ######################################################################################################################
+# Solution
+# ######################################################################################################################
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        res = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            res[index] += char
+            if index == 0: step = 1
+            elif index == numRows - 1: step = -1
+            index += step
+
+        return ''.join(res)
