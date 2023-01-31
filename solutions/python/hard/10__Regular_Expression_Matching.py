8845c51c5a54a1c1b80fbf233be6cbfdc7d60e71
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2023-01-31 18:21:24 +0300

    Summary

---
+++ solutions/python/hard/10__Regular_Expression_Matching.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+10. Regular Expression Matching
+"""
+
+# ######################################################################################################################
+# Solution
+# ######################################################################################################################
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+
+        return dp[m][n]
