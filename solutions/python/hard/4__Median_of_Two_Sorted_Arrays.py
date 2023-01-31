3c3696b5f5943bda92f3f382f408ed40c3334c47
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2023-01-31 17:18:12 +0300

    Update 4__Median_of_Two_Sorted_Arrays.py

--- solutions/python/hard/4__Median_of_Two_Sorted_Arrays.py
+++ solutions/python/hard/4__Median_of_Two_Sorted_Arrays.py
@@ -35,9 +35,11 @@ class Solution:
                 if i == 0: max_of_left = nums2[j - 1]
                 elif j == 0: max_of_left = nums1[i - 1]
                 else: max_of_left = max(nums1[i - 1], nums2[j - 1])
-                if (m + n) % 2 == 1:
-                    return max_of_left
+
+                if (m + n) % 2 == 1: return max_of_left
+
                 if i == m: min_of_right = nums2[j]
                 elif j == n: min_of_right = nums1[i]
                 else: min_of_right = min(nums1[i], nums2[j])
+
                 return (max_of_left + min_of_right) / 2
